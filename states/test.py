807c595b06b6a7ea754da89e7ad343a71038a7a0
Himchenhim <46593197+Himchenhim@users.noreply.github.com>
2020-09-20 11:52:11 +0300

    useless state, not suit for template
    fix

--- states/test.py
+++
@@ -1,8 +0,0 @@
-from aiogram.dispatcher.filters.state import State, StatesGroup
-
-
-class Form(StatesGroup):
-    Q1_name = State()
-    Q2_email = State()
-    Q3_phone = State()
-    Q4 = State()
