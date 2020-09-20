1e0a2adbab520139ca967049b4302fb8c184a38e
Himchenhim <46593197+Himchenhim@users.noreply.github.com>
2020-09-20 11:47:34 +0300

    deleted unnecessary things
    fix

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -3,9 +3,5 @@ from aiogram import types
 async def set_default_commands(dp):
     await dp.bot.set_my_commands([
         types.BotCommand("start", "Запуск бота"),
-        types.BotCommand("biba", "Узнать свой размер"),
-        types.BotCommand("gay", "Проверка на гея"),
-        types.BotCommand("ro", "Выдача режима - 'read_only'"),
-        types.BotCommand("dead_inside", "Проверка юности"),
-        types.BotCommand("mmr","Проверка ММР-а")
+        types.BotCommand("help","Помощь при работе с ботом")
     ])
